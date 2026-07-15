fa34fad64a42f30ba7be3c356f12ff713ae4ae86
08EAGM94 <08edgar.allan94@gmail.com>
2026-07-15 12:43:36 -0600

    update images

--- sosv6-docker-run/restore.sh
+++ sosv6-docker-run/restore.sh
@@ -17,19 +17,34 @@ if [ $? -ne 0 ]; then
     exit 1  
 fi
 
-echo "Verificando si la base de datos 'SOSv5DB' ya existe..."
+echo "Verificando si la base de datos 'SOSv6DB' ya existe..."
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "${MSSQL_SA_PASSWORD}" -C -Q \
-"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'SOSv5DB')
+"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'SOSv6DB')
 BEGIN
-    PRINT 'SOSv5DB no encontrada. Iniciando restauración desde backup...'
-    RESTORE DATABASE SOSv5DB 
-    FROM DISK = '/var/opt/mssql/backup/SOSv5DB.bak' 
-    WITH MOVE 'SOSv5DB' TO '/var/opt/mssql/data/SOSv5DB.mdf', 
-         MOVE 'SOSv5DB_log' TO '/var/opt/mssql/data/SOSv5DB.ldf'
+    PRINT 'SOSv6DB no encontrada. Iniciando restauración desde backup...'
+    RESTORE DATABASE SOSv6DB 
+    FROM DISK = '/var/opt/mssql/backup/SOSv6DB.bak' 
+    WITH MOVE 'SOSv6DB' TO '/var/opt/mssql/data/SOSv6DB.mdf', 
+         MOVE 'SOSv6DB_log' TO '/var/opt/mssql/data/SOSv6DB.ldf'
 END
 ELSE
 BEGIN
-    PRINT 'SOSv5DB ya existe en el volumen persistente. Saltando restauración.'
+    PRINT 'SOSv6DB ya existe en el volumen persistente. Saltando restauración.'
 END"
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "${MSSQL_SA_PASSWORD}" -N -C -Q "USE SOSv5DB; UPDATE Usuarios SET Privilegio = 'Admin' WHERE Alias = 'ELENA86';"
+
+echo "Verificando si la base de datos 'SOSTest' ya existe..."
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "${MSSQL_SA_PASSWORD}" -C -Q \
+"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'SOSTest')
+BEGIN
+    PRINT 'SOSTest no encontrada. Iniciando restauración desde backup...'
+    RESTORE DATABASE SOSTest 
+    FROM DISK = '/var/opt/mssql/backup/SOSTest.bak' 
+    WITH MOVE 'SOSTest' TO '/var/opt/mssql/data/SOSTest.mdf', 
+         MOVE 'SOSTest_log' TO '/var/opt/mssql/data/SOSTest.ldf'
+END
+ELSE
+BEGIN
+    PRINT 'SOSTest ya existe en el volumen persistente. Saltando restauración.'
+END"
+
 wait
